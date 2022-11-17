8fecda5ad46267b94c15a4a702a192f21edd62ea
André Quaresma <andre.quaresma@solera.com>
2022-11-17 10:26:32 +0100

    Spring setup

---
+++ java-final-assessment/src/main/java/Final/Assessment/javafinalassessment/JavaFinalAssessmentApplication.java
@@ -0,0 +1,13 @@
+package Final.Assessment.javafinalassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFinalAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFinalAssessmentApplication.class, args);
+	}
+
+}
---
+++ java-final-assessment/src/test/java/Final/Assessment/javafinalassessment/JavaFinalAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package Final.Assessment.javafinalassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFinalAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
